928a5730c74a3b6e983855965c03d4062a8f4a7c
ErrorDebug404 <malhotrakk121@gmail.com>
2024-07-21 18:10:12 +0530

    Add files via upload
    firstEdit

---
+++ Demo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class transacNode{
+    string sender;
+    string receiver;
+    int transacCode;
+
+    //constructors
+
+
+
+    //write the methods as well for the class mentioned above
+};
+
